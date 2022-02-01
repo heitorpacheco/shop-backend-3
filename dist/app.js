e07964dcf92b704c9270f387e3cea0aab726b8c8
Heitor Pacheco <heeitorpacheco@gmail.com>
2022-02-01 17:12:40 -0300

    .

---
+++ dist/app.js
@@ -0,0 +1,22 @@
+"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }require('dotenv/config');
+
+var _express = require('express'); var _express2 = _interopRequireDefault(_express);
+var _routes = require('./routes'); var _routes2 = _interopRequireDefault(_routes);
+var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
+
+class App {
+  constructor() {
+    this.server = _express2.default.call(void 0, );
+  }
+
+  middlewares() {
+    this.server.use(_cors2.default.call(void 0, ));
+    this.server.use(_express2.default.json());
+  }
+
+  routes() {
+    this.server.use(_routes2.default);
+  }
+}
+
+exports. default = new App().server;
---
+++ dist/routes.js
@@ -0,0 +1,10 @@
+"use strict";Object.defineProperty(exports, "__esModule", {value: true});var _express = require('express');
+
+const routes = new (0, _express.Router)();
+
+// routes.get('/get', (req, res) => {
+//   return res.json({ ok: true });
+// });
+
+
+exports. default = routes;
