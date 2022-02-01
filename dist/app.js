b7b5b220d9a1b8260dc8093376d0a33ded3e00d3
Heitor Pacheco <heeitorpacheco@gmail.com>
2022-02-01 17:54:50 -0300

    .

--- dist/app.js
+++ dist/app.js
@@ -7,6 +7,8 @@ var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
 class App {
   constructor() {
     this.server = _express2.default.call(void 0, );
+
+    this.routes();
   }
 
   middlewares() {
