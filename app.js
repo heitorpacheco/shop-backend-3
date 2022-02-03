c1b6e8a636b6f10d0a92272e83fb59210cb639dc
Heitor Pacheco <heeitorpacheco@gmail.com>
2022-02-03 11:53:24 -0300

    .

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const { mockData } = require("./mockData");
+const Sequelize = require("sequelize");
+const { QueryTypes } = require("sequelize");
 
 const app = express();
 const port = process.env.port || 3000;
@@ -19,6 +21,38 @@ app.get("/social/:id", (req, res) => {
   var id = req.params.id;
   res.send(mockData[id]);
 });
+
+app.get('/rotatestedb', async (req, res) => {
+  const sequelize = new Sequelize(
+    'shop',
+    'postgres',
+    'pg2022#OffStore!',
+    {
+      host: 'offstore.cpnztdd8ok7k.sa-east-1.rds.amazonaws.com',
+      dialect: 'postgres',
+
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+      },
+    }
+  );
+
+  const response = await sequelize.query(
+    `
+    SELECT * FROM users where id = 1
+  `,
+    {
+      type: QueryTypes.SELECT,
+    }
+  );
+
+  return res.json(response);
+});
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
